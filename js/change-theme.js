605ed6ff224fbd74acae2835591424f99abe43db
Marcelo Augusto <80494196+marceloamellopaixao@users.noreply.github.com>
2024-02-19 21:54:00 -0300

    v2.0.3

--- js/change-theme.js
+++ js/change-theme.js
@@ -1,17 +1,24 @@
-const themeChangeButton = document.querySelector('.theme-change');
-const body = document.querySelectorAll( 'body' )[0];
+const themeChangeButton = document.querySelector(".theme-change");
+const body = document.querySelectorAll("body")[0];
+let linksHeader = document.querySelectorAll(".cabecalho-a");
 
-themeChangeButton.addEventListener('click', () => {
-    body.classList.toggle('dark-mode');
+themeChangeButton.addEventListener("click", () => {
+  body.classList.toggle("dark-mode");
 
-    // Save the current theme preference to local storage
-    const isDarkMode = body.classList.contains('dark-mode');
-    localStorage.setItem('dark-mode', isDarkMode);
+  if ((onclick = true)) {
+    for (let index = 0; index < linksHeader.length; index++) {
+      linksHeader[index].style.color = "#fff";
+    }
+  } 
+
+  // Salve o tema de preferencia do usuário no armazenamento local.
+  const isDarkMode = body.classList.contains("dark-mode");
+  localStorage.setItem("dark-mode", isDarkMode);
 });
 
-// Check the user's theme preference from local storage
-const savedDarkMode = localStorage.getItem('dark-mode');
+// Cheque o tema de preferência do usuário do armazenamento local
+const savedDarkMode = localStorage.getItem("dark-mode");
 
-if (savedDarkMode === 'true') {
-    body.classList.add('dark-mode');
+if (savedDarkMode === "true") {
+  body.classList.add("dark-mode");
 }
