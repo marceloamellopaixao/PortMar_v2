a9d56d09a656a87290807ca4cdd01846fbab122d
marceloamellopaixao <marceloamellopaixao@gmail.com>
2024-03-07 16:49:41 -0300

    v2.0.7

---
+++ public/js/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDWMRiaeeFM_bkf084wmFC3wd_7eQJ2j_c",
+  authDomain: "portmar.firebaseapp.com",
+  databaseURL: "https://portmar-default-rtdb.firebaseio.com",
+  projectId: "portmar",
+  storageBucket: "portmar.appspot.com",
+  messagingSenderId: "1029502061723",
+  appId: "1:1029502061723:web:5ed2bd29e0b44adba40a3e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
